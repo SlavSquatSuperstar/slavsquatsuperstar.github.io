1142f292a79b90cbf4bfdef63a63320f20b88bf7
Eric Weng <ericweng03@gmail.com>
2020-10-14 10:26:31 -0400

    Add styles and scripts folder
    change email to @chb-enterprises
    add JS to "apply" page

---
+++ scripts/apply.js
@@ -0,0 +1,9 @@
+var applicationForm = document.getElementById("applicationForm")
+
+function submitApplication() {
+	window.alert("You have submitted your application.")
+}
+
+function showApplicationForm() {
+	applicationForm.style.display = "block"
+}
